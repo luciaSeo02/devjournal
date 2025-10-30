39a369c38e7b064db14b390d5295e0ce35d83848
Lucía Seoane <luciaseo20@gmail.com>
2025-10-30 18:54:27 +0100

    Update icon and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,17 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import "./Footer.scss";
 export default function Footer() {
   return (
     <footer className="app-footer">
       <p>Made with ❤️ by Lucía Seoane</p>
+      <div className="social-links">
+        <a href="https://github.com/luciaSeo02" target="_blank">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/lucia-seo/" target="_blank">
+          <FaLinkedin />
+        </a>
+      </div>
     </footer>
   );
 }
